e1290fdbeb8b0b5accbc47bbd63e7ac0d65e7c57
AhnafTahmid98 <tahmidahnaf998@gmail.com>
2025-11-26 07:21:12 +0200

    savo_speech

--- tools/scripts/env_llm.sh
+++ tools/scripts/env_llm.sh
@@ -3,7 +3,7 @@
 # This is the ONLY file you edit when the LLM server IP/port changes.
 
 # Replace this IP with the hotspot IP of your LLM server (PC or MacBook)
-export LLM_SERVER_URL="http://10.108.98.173:8000"
+export LLM_SERVER_URL="http://10.108.98.119:8000"
 
 echo "[env_llm] LLM_SERVER_URL = ${LLM_SERVER_URL}"
  
