f4109f725c41708304aadf883948f90643ab168f
AhnafTahmid98 <tahmidahnaf998@gmail.com>
2026-07-02 22:16:11 +0300

    savo_perception testing

--- savo_ws/src/shared/savo_perception/include/savo_perception/vl53_mux_node.hpp
+++ savo_ws/src/shared/savo_perception/include/savo_perception/vl53_mux_node.hpp
@@ -1,7 +1,10 @@
 #ifndef SAVO_PERCEPTION__VL53_MUX_NODE_HPP_
 #define SAVO_PERCEPTION__VL53_MUX_NODE_HPP_
 
+#include <atomic>
+#include <chrono>
 #include <memory>
+#include <mutex>
 #include <optional>
 #include <string>
 
@@ -40,12 +43,25 @@ struct SAVO_PERCEPTION_PUBLIC Vl53MuxNodeConfig
 
   bool publish_nan_on_error{constants::kPublishNanOnErrorDefault};
   bool startup_fail_is_fatal{constants::kStartupFailIsFatalDefault};
+
+  double stale_timeout_s{0.50};
+};
+
+struct SAVO_PERCEPTION_PUBLIC Vl53LatestState
+{
+  Vl53l1xReading left;
+  Vl53l1xReading right;
+
+  std::chrono::steady_clock::time_point stamp{std::chrono::steady_clock::now()};
+  std::string driver_error;
+  bool has_update{false};
 };
 
 class SAVO_PERCEPTION_PUBLIC Vl53MuxNode : public rclcpp::Node
 {
 public:
   explicit Vl53MuxNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+  ~Vl53MuxNode() override;
 
 private:
   void declare_parameters();
@@ -55,8 +71,13 @@ private:
   void start_driver();
   void stop_driver();
 
+  void start_worker();
+  void request_worker_stop();
+  void worker_loop();
+
   void on_timer();
 
+  void publish_latest();
   void publish_reading(
     const rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr & publisher,
     const Vl53l1xReading & reading);
@@ -65,17 +86,24 @@ private:
     const rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr & publisher,
     const std::optional<double> & distance_m);
 
+  [[nodiscard]] bool latest_is_stale(const Vl53LatestState & latest) const;
   [[nodiscard]] Vl53MuxPairConfig make_driver_config() const;
 
   Vl53MuxNodeConfig config_{};
 
-  std::unique_ptr<Vl53MuxPairDriver> driver_;
+  std::shared_ptr<Vl53MuxPairDriver> driver_;
   std::string driver_error_;
 
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr left_pub_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr right_pub_;
 
   rclcpp::TimerBase::SharedPtr timer_;
+
+  std::atomic_bool worker_stop_requested_{false};
+  std::atomic_bool worker_running_{false};
+
+  mutable std::mutex latest_mutex_;
+  Vl53LatestState latest_;
 };
 
 }  // namespace savo_perception
