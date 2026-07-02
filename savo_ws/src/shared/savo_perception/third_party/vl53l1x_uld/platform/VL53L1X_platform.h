3391abcaaaad32ffb1ebe49b14eceb8be621577e
AhnafTahmid98 <tahmidahnaf998@gmail.com>
2026-07-02 20:07:54 +0300

    savo_perception testing

--- savo_ws/src/shared/savo_perception/third_party/vl53l1x_uld/platform/VL53L1X_platform.h
+++ savo_ws/src/shared/savo_perception/third_party/vl53l1x_uld/platform/VL53L1X_platform.h
@@ -1,26 +1,6 @@
-#ifndef VL53L1X_PLATFORM_H_
-#define VL53L1X_PLATFORM_H_
+#ifndef SAVO_PERCEPTION_THIRD_PARTY_VL53L1X_ULD_PLATFORM_VL53L1X_PLATFORM_H_
+#define SAVO_PERCEPTION_THIRD_PARTY_VL53L1X_ULD_PLATFORM_VL53L1X_PLATFORM_H_
 
-#include <stdint.h>
+#include "vl53l1_platform.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void VL53L1X_SetI2CBus(int bus);
-
-int8_t VL53L1X_WriteMulti(uint16_t dev, uint16_t index, uint8_t * pdata, uint32_t count);
-int8_t VL53L1X_ReadMulti(uint16_t dev, uint16_t index, uint8_t * pdata, uint32_t count);
-int8_t VL53L1X_WrByte(uint16_t dev, uint16_t index, uint8_t data);
-int8_t VL53L1X_WrWord(uint16_t dev, uint16_t index, uint16_t data);
-int8_t VL53L1X_WrDWord(uint16_t dev, uint16_t index, uint32_t data);
-int8_t VL53L1X_RdByte(uint16_t dev, uint16_t index, uint8_t * data);
-int8_t VL53L1X_RdWord(uint16_t dev, uint16_t index, uint16_t * data);
-int8_t VL53L1X_RdDWord(uint16_t dev, uint16_t index, uint32_t * data);
-int8_t VL53L1X_WaitMs(uint16_t dev, int32_t wait_ms);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif  // VL53L1X_PLATFORM_H_
+#endif  // SAVO_PERCEPTION_THIRD_PARTY_VL53L1X_ULD_PLATFORM_VL53L1X_PLATFORM_H_
